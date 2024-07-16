d4d57e389f089c37f98daa022c58a17745ef1ab6
Cosmin Popovici <hellocosmin@gmail.com>
2024-07-16 09:33:21 +0300

    refactor: use local boxShadow plugin
    removes tailwindcss-box-shadow dependency

--- src/index.test.js
+++ src/index.test.js
@@ -71,6 +71,26 @@ test('borderSpacing', () => {
   })
 })
 
+test('boxShadow', () => {
+  const config = {
+    content: [
+      {
+        raw: String.raw`
+          <div class="shadow"></div>
+        `
+      }
+    ],
+  }
+
+  return run(config).then(result => {
+    expect(result.css).toMatchCss(String.raw`
+      .shadow {
+        box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px -1px rgba(0, 0, 0, 0.1)
+      }
+    `)
+  })
+})
+
 test('textDecoration', () => {
   const config = {
     content: [
